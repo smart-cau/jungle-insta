56885cf30dab86ec0775b5095773ed8895a2cba0
HyeonWooJung <1995hyunwoo@gmail.com>
2024-10-08 23:40:25 +0900

    feat: main.ts에 swagger 등록

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Board project')
+    .setDescription('The board API description')
+    .setVersion('1.0')
+    .addTag('board')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  const serverConfig: { port: number } = config.get('server');
+
+  const port = serverConfig.port;
+  await app.listen(port);
 }
 bootstrap();
