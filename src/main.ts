50f9ef8bece14f334af846d0df1ac83df9c867df
HyeonWooJung <1995hyunwoo@gmail.com>
2024-10-09 21:25:16 +0900

    feat: add login feature in swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { VersioningType } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,13 +16,29 @@ async function bootstrap() {
     defaultVersion: '1',
   });
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Jungle Insta project')
     .setDescription('The Jungle Insta API description')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+      },
+      'accessToken',
+    )
     .build();
+  const swaggerCustomOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, swaggerCustomOptions);
 
   const serverConfig: { port: number } = config.get('server');
 
